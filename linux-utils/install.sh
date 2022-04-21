00c07b6e149ea1442ef7ccf0c064a28d14171e6f
Jeremy Kahn <jeremyckahn@gmail.com>
2022-04-21 08:40:12 -0500

    improve cleanup from previous stow operations

--- linux-utils/install.sh
+++ linux-utils/install.sh
@@ -7,12 +7,15 @@ cd ~/dotfiles
 make homebrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-make tools dotfiles_cleanup
+# Remove any links from a previous stow operation
+[ -L ~/.profile ] && rm ~/.profile
+[ -L ~/.bash_profile ] && rm ~/.bash_profile
 
+# Keep pre-existing configuration as environment-specific files
 [ -f ~/.profile ] && [ ! -f ~/.env.profile ] && mv ~/.profile ~/.env.profile
 [ -f ~/.bash_profile ] && [ ! -f ~/.env.bash_profile ] && mv ~/.bash_profile ~/.env.bash_profile
 
-make bashrc dotfiles neovim
+make tools bashrc dotfiles neovim
 
 echo 'Done! Consider running:'
 echo
