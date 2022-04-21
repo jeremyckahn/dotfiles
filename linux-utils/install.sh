7a757c0d41e5f36d0cc25010da6cbd9e514f0625
Jeremy Kahn <jeremyckahn@gmail.com>
2022-04-20 21:45:39 -0500

    source homebrew during Linux install

--- linux-utils/install.sh
+++ linux-utils/install.sh
@@ -3,11 +3,13 @@
 sudo apt install make
 cd dotfiles
 make homebrew
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Temporarily install system-level node for one-time-setup
 brew install "node@16"
 make bashrc tools dotfiles neovim
 brew uninstall "node@16"
+source ~/.bash_profile
 
 echo 'Done! Consider running:'
 echo
