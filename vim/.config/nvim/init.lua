4e0ec06ec8f383a73a6275a7725c7285848c32a0
Jeremy Kahn <me@jeremyckahn.com>
2025-01-19 19:04:14 -0600

    feat(nvim): vim.o.relativenumber = false

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -6,3 +6,4 @@ vim.cmd([[colo github_dark_default]])
 vim.api.nvim_create_user_command("Reload", [[execute "source ~/.config/nvim/init.lua"]], { desc = "Reload the config" })
 
 vim.o.wrap = true
+vim.o.relativenumber = false
