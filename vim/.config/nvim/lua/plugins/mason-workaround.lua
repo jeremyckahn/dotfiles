dbd3ab5395cacc40085731592fea19b7dd3a9ae6
Jeremy Kahn <me@jeremyckahn.com>
2025-05-08 21:19:02 -0500

    chore(nvim): update lazyvim

---
+++ vim/.config/nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,7 @@
+-- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+--
+-- FIXME: Remove when https://github.com/LazyVim/LazyVim/issues/6039 is fixed
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
